390e8b09701369f54ec6e3747088b85d81de2ec2
Rajssss <sssraj.sssraj@gmail.com>
2020-10-03 13:33:29 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hacktoberfest_workshop_demo
+Demo repositoryfor Hacktoberfest 2020 Workshop. All Pull-Requests will be labled invalid in this repository.
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//simple calculator
+int main(void)
+{
+    int x, y, result = 0;
+    char opt;
+
+    //input two numbers
+    printf("Enter a value of x => ");
+    scanf("%d", &x);
+    printf("Enter a value of y => ");
+    scanf("%d", &y);
+
+    //input operator
+    fflush(stdin);
+    printf("Operator =>");
+    scanf("%c", &opt);
+
+    //check the operator
+    switch (opt)
+    {
+        case '+':  //add it
+            result = x + y; break;
+    }
+
+    //print the result
+    printf("Result => %d", result);
+
+    return 0;
+}
